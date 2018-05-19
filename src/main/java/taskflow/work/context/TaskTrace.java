e0599423f571a1a16306c5dfc4faec5b7cee7121
baileyfu <bailey_fu@163.com>
2018-05-19 19:53:54 +0800

    update

--- src/main/java/taskflow/work/context/TaskTrace.java
+++ src/main/java/taskflow/work/context/TaskTrace.java
@@ -1,31 +1,30 @@
 package taskflow.work.context;
 
 /**
- * 记录bus的运行路径，方便打印日志，了解业务调用流程和快照
- * Created by lizhou on 2017/4/8/008.
+ * 记录bus的运行路径，方便打印日志，了解业务调用流程和快照 Created by lizhou on 2017/4/8/008.
  */
 public class TaskTrace {
-    private String stationName;
-    private String contextInfo;
+	private String taskName;
+	private String contextInfo;
 
-    public TaskTrace(String stationName, String contextInfo) {
-        this.stationName = stationName;
-        this.contextInfo = contextInfo;
-    }
+	public TaskTrace(String taskName, String contextInfo) {
+		this.taskName = taskName;
+		this.contextInfo = contextInfo;
+	}
 
-    public String getStationName() {
-        return stationName;
-    }
+	public String getTaskName() {
+		return taskName;
+	}
 
-    public void setStationName(String stationName) {
-        this.stationName = stationName;
-    }
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
 
-    public String getContextInfo() {
-        return contextInfo;
-    }
+	public String getContextInfo() {
+		return contextInfo;
+	}
 
-    public void setContextInfo(String contextInfo) {
-        this.contextInfo = contextInfo;
-    }
+	public void setContextInfo(String contextInfo) {
+		this.contextInfo = contextInfo;
+	}
 }
