279d1c68f3140e288429eca1c53981858f40c16f
bailey <bailey_fu@163.com>
2022-04-25 15:19:15 +0800

    set result

--- src/test/java/demo/task/CalNumber.java
+++ src/test/java/demo/task/CalNumber.java
@@ -6,6 +6,7 @@ public class CalNumber {
 
 	public void cal(WorkContext workContext) {
 		System.out.println("sub work execute...");
+		workContext.setResult("RESULT");
 	}
 	
 	public void someMethod() {
